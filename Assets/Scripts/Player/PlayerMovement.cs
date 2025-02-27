ccb4b837d5536a3407dd26764488e268de40d396
KongAnVjpPro <phamcongan2003@gmail.com>
2025-02-28 00:03:59 +0700

    slash effect 14.46

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
@@ -14,9 +15,9 @@ public class PlayerMovement : AnMonobehaviour
     [Header("Vertical Movement Settings:")]
     [SerializeField] protected float jumpForce = 10;
     private float jumpBufferCounter = 0;
-    [SerializeField] private float jumpBufferFrames = 60;
+    [SerializeField] private float jumpBufferFrames = 0.1f;
     private float coyoteTimeCounter = 0;
-    [SerializeField] private float coyoteTime = 0.1f;
+    [SerializeField] private float coyoteTime = 0.15f;
     private int airJumpCounter = 0;
     [SerializeField] private int maxAirJumps = 1;
     [Space(5)]
@@ -36,11 +37,23 @@ public class PlayerMovement : AnMonobehaviour
     [SerializeField] private GameObject dashEffect;
     [Space(5)]
 
+    [Header("Attacking")]
+    [SerializeField] float timeBetweenAttack = 0.5f;
+    float timeSinceAttack = 0f;
+    bool attack = false;
+    [SerializeField] Transform sideAttackTransform, upAttackTransform, downAttackTransform;
+    [SerializeField] Vector2 sideAttackArea, upAttackArea, downAttackArea;
+    [SerializeField] LayerMask attackableLayer;
+    [SerializeField] float damage = 1f;
+    [SerializeField] GameObject slashEffect;
+
+
+
     private bool dashed = false;
     PlayerStateList pState;
     [SerializeField] protected Animator anim;
     [SerializeField] protected Rigidbody2D body;
-    private float xAxis;
+    private float xAxis, yAxis;
     private bool canDash = true;
     private float gravity;
 
@@ -75,6 +88,14 @@ public class PlayerMovement : AnMonobehaviour
         if (this.pState != null) return;
         this.pState = GetComponent<PlayerStateList>();
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(sideAttackTransform.position, sideAttackArea);
+        Gizmos.DrawWireCube(upAttackTransform.position, upAttackArea);
+        Gizmos.DrawWireCube(downAttackTransform.position, downAttackArea);
+    }
     private void Update()
     {
         GetInputs();
@@ -84,10 +105,13 @@ public class PlayerMovement : AnMonobehaviour
         Jump();
         Flip();
         StartDash();
+        Attack();
     }
     protected virtual void GetInputs()
     {
         xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+        attack = Input.GetMouseButtonDown(0);
     }
     protected virtual void Move()
     {
@@ -128,6 +152,53 @@ public class PlayerMovement : AnMonobehaviour
 
 
     }
+    void Attack()
+    {
+        timeSinceAttack += Time.deltaTime;
+        if (attack && timeSinceAttack >= timeBetweenAttack)
+        {
+            timeSinceAttack = 0;
+            anim.SetTrigger("Attacking");
+
+            if (yAxis == 0 || yAxis < 0 && Grounded())
+            {
+                Hit(sideAttackTransform, sideAttackArea);
+                Instantiate(slashEffect, sideAttackTransform);
+            }
+            else if (yAxis > 0)
+            {
+                Hit(upAttackTransform, upAttackArea);
+                SlashEffectAtAngle(slashEffect, 90, upAttackTransform);
+            }
+            else if (yAxis < 0 && !Grounded())
+            {
+                Hit(downAttackTransform, downAttackArea);
+                SlashEffectAtAngle(slashEffect, -90, downAttackTransform);
+            }
+        }
+    }
+    void Hit(Transform _attackTransform, Vector2 _attackArea)
+    {
+        Collider2D[] objectToHit = Physics2D.OverlapBoxAll(_attackTransform.position, _attackArea, 0, attackableLayer);
+
+        if (objectToHit.Length > 0)
+        {
+            Debug.Log("HIt");
+        }
+        for (int i = 0; i < objectToHit.Length; i++)
+        {
+            if (objectToHit[i].GetComponent<Enemy>() != null)
+            {
+                objectToHit[i].GetComponent<Enemy>().EnemyHit(damage);
+            }
+        }
+    }
+    void SlashEffectAtAngle(GameObject _slashEffect, int _effectAngle, Transform _attackTransform)
+    {
+        _slashEffect = Instantiate(_slashEffect, _attackTransform);
+        _slashEffect.transform.eulerAngles = new Vector3(0, 0, _effectAngle);
+        _slashEffect.transform.localScale = new Vector2(transform.localScale.x, transform.localScale.y);
+    }
     public bool Grounded()
     {
         if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround) ||
@@ -157,23 +228,25 @@ public class PlayerMovement : AnMonobehaviour
     }
     void Jump()
     {
-        if (Input.GetButtonUp("Jump") && body.velocity.y > 0)
+        if (Input.GetButtonUp("Jump") && body.velocity.y > 0)//tha nup jump
         {
             body.velocity = new Vector2(body.velocity.x, 0);
             pState.jumping = false;
         }
-        if (!pState.jumping)
-        {
-            if (jumpBufferCounter > 0 && coyoteTimeCounter > 0)//coyoteTime la khoang thoi gian giua 2 lan nhay
-            {                                                   //jumpBufferCounter la bien check xem co phai nhan nut nhay ko
+        if (!pState.jumping)//neu ko nhay
+        {       //o day nghia la neu vua roi mat dat khoang thoi gian coyotetime > 0 va nhan nut nhay thi se nhay
+            if (jumpBufferCounter > 0 && coyoteTimeCounter > 0)//coyoteTime la khoang thoi gian khi roi mat dat
+            {                                                   //jumpBuffer khoang thoi gian nhan nut som
                 body.velocity = new Vector2(body.velocity.x, jumpForce);
                 pState.jumping = true;
+
             }
             else if (!Grounded() && airJumpCounter < maxAirJumps && Input.GetButtonDown("Jump"))
             {
                 body.velocity = new Vector2(body.velocity.x, jumpForce);
                 pState.jumping = true;
                 airJumpCounter++;
+
             }
 
         }
@@ -198,7 +271,7 @@ public class PlayerMovement : AnMonobehaviour
         }
         else
         {
-            jumpBufferCounter -= Time.deltaTime * 10;
+            jumpBufferCounter -= Time.deltaTime;
         }
     }
 }
